84e3cdda169ce33261a992881b17bc28d48e456a
Joaquin Juan Toledo <joaquin.j.toledo@gmail.com>
2016-12-10 20:03:55 +0100

    [DEV] Create RpiManager class

---
+++ RPiManager.cpp
@@ -0,0 +1,14 @@
+/*
+ * RPiManager.cpp
+ *
+ *  Created on: Dec 6, 2016
+ *      Author: joaquin
+ */
+#include "RpiManager.h"
+
+RPiManager::RPiManager():display(Display())
+{
+
+}
+
+
---
+++ RpiManager.h
@@ -0,0 +1,24 @@
+/*
+ * RpiManager.h
+ *
+ *  Created on: Dec 6, 2016
+ *      Author: joaquin
+ */
+
+#ifndef RPIMANAGER_H_
+#define RPIMANAGER_H_
+
+#include "Display.h"
+
+class RPiManager
+{
+public:
+    RPiManager();
+private:
+    Display display;
+};
+
+
+
+
+#endif /* RPIMANAGER_H_ */
